9fe55e193134db275b419a9de17608b9b6ac94b8
Pietrek14 <dawidekpe@gmail.com>
2022-05-17 09:30:20 +0200

    initial commit
    Co-authored-by: staszad <staszad@users.noreply.github.com>

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        negro
+      </header>
+      <main></main>
+    </div>
+  )
+}
+
+export default App
