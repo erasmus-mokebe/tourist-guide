31bc09231160d5c0a7ac56e82f30e6794842d219
szaszkowskap20 <m.szaszkowska.p20@gmail.com>
2022-05-19 11:14:03 +0300

    change description

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -6,7 +6,7 @@ const Cards = () => {
       <CardSingle
         img='src/assets/img/paths/beach.jpg'
         name='Incredible Beaches'
-        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed luctus ante. Suspendisse mattis eget sem sed dapibus. In sed quam urna. Donec facilisis, odio vel scelerisque sollicitudin.'
+        description='Lorem ipsum dolor sit ametitur sed luctus ante. Suspendisse mattis eget sem sed dapibus. In sed quam urna. Donec facilisis, odio vel scelerisque sollicitudin.'
       />
     </section>
   );
