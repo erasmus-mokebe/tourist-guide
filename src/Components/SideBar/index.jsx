73c7f9f9ddc2638795e1e9669744864505e04e30
Pietrek14 <dawidekpe@gmail.com>
2022-05-23 09:37:28 +0200

    fixed problem

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -14,7 +14,7 @@ const SideBar = ({ locationId }) => {
   const sideBar = (
     <section
       id="sidebar"
-      className="hidden sm:flex w-1/3 h-full bg-white justify-center"
+      className="flex w-full sm:w-1/3 h-full bg-white justify-center z-40"
     >
       <div className="w-4/5">
         <SidebarSingle
