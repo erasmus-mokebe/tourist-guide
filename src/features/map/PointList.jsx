f62573821cf5daba2ecff47be97dd0e1461de803
Pietrek14 <dawidekpe@gmail.com>
2022-05-24 14:10:02 +0300

    you can now filter places by type

--- src/features/map/PointList.jsx
+++ src/features/map/PointList.jsx
@@ -3,14 +3,18 @@ import { useSelector } from "react-redux";
 
 export const PointList = ({ onMarkerClick }) => {
   const locations = useSelector((state) => state.locations.locations);
+  const locationsFilters = useSelector((state) => state.locations.filters);
 
   if (!locations) return <></>;
 
-  return locations.map(({ id, place }) => (
-    <Marker
-      key={id}
-      position={place.coords}
-      eventHandlers={{ click: () => onMarkerClick(id) }}
-    />
-  ));
+  return locations.map(
+    ({ id, place, type }) =>
+      (locationsFilters.type === "all" || locationsFilters.type === type) && (
+        <Marker
+          key={id}
+          position={place.coords}
+          eventHandlers={{ click: () => onMarkerClick(id) }}
+        />
+      )
+  );
 };
