ac8d0d0447ae5a712f779c5ea5d11ca7085d5779
LordPrinz <plazinski2902@gmail.com>
2022-05-24 10:07:50 +0300

    Change get points method

--- src/pages/Browser/index.jsx
+++ src/pages/Browser/index.jsx
@@ -1,12 +1,9 @@
 import SideBar from "../../Components/SideBar";
 import Map from "../../Components/Map";
-import CurrentLocationButton from "../../Components/CurrentLocationButton";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setLocations } from "../../store/slices/locationsSlice";
-import { openSideBar } from "../../store/slices/sideBarSlice";
 const Browser = () => {
-  const [currentPointId, setCurrentPointId] = useState("");
   const dispatch = useDispatch();
   const endpoint = "locations";
   const port = 8080;
