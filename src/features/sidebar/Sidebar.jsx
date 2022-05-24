0feb9c21bd8012e70340dc11e165133a55aa41bb
Pietrek14 <dawidekpe@gmail.com>
2022-05-24 09:25:13 +0200

    sidebar now actually works on mobile

--- src/features/sidebar/Sidebar.jsx
+++ src/features/sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 export const SideBar = ({ children }) => {
-  const isOpen = useSelector(state => state.sideBar.opened);
+  const isOpen = useSelector((state) => state.sideBar.opened);
 
   const sideBar = (
     <section
-      id='sidebar'
-      className='flex min-w-[400px] w-[400px] h-full bg-white justify-center z-40'
+      id="sidebar"
+      className="flex w-full lg:min-w-[400px] lg:w-[400px] h-full bg-white justify-center z-40"
     >
       {children}
     </section>
