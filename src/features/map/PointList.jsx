1523d09d5872de206d873af3a06865a30f1b38db
Pietrek14 <dawidekpe@gmail.com>
2022-05-25 11:23:21 +0300

    added custom map markers for different types of locations

--- src/features/map/PointList.jsx
+++ src/features/map/PointList.jsx
@@ -1,20 +1,114 @@
 import { Marker } from "react-leaflet";
+import { divIcon } from "leaflet";
 import { useSelector } from "react-redux";
+import { renderToString } from "react-dom/server";
+import { ReactElement } from "react";
+
+import RestaurantIcon from "../../assets/icons/cutlery.svg";
+import MuseumIcon from "../../assets/icons/museum.svg";
+import ParkingIcon from "../../assets/icons/parking.svg";
+import MapMarker from "../../assets/icons/map-marker.svg";
+
+const GetIconImage = (type) => {
+	console.log(type);
+	switch (type) {
+		case "parking":
+			return ParkingIcon;
+		case "restaurant":
+			return RestaurantIcon;
+		case "museum":
+			return MuseumIcon;
+		default:
+			return null;
+	}
+};
+
+const GetIconColor = (type) => {
+	switch (type) {
+		case "parking":
+			return "#00bcd4";
+		case "restaurant":
+			return "#005e54";
+		case "museum":
+			return "#c2bb00";
+		default:
+			return "#ff9800";
+	}
+};
+
+const GetIcon = (type) => {
+	const icon = GetIconImage(type);
+	if (icon) {
+		return (
+			<div
+				className="w-full h-full"
+				style={{
+					maskImage: `url("${MapMarker}")`,
+					maskSize: "contain",
+					maskRepeat: "no-repeat",
+					maskPosition: "center",
+					WebkitMaskImage: `url("${MapMarker}")`,
+					WebkitMaskSize: "contain",
+					WebkitMaskRepeat: "no-repeat",
+					WebkitMaskPosition: "center",
+				}}
+			>
+				<div
+					className="w-full h-full"
+					style={{
+						backgroundColor: GetIconColor(type),
+					}}
+				>
+					<div className="flex justify-center items-center w-full aspect-square p-1">
+						<div
+							className="w-full h-full"
+							style={{
+								maskImage: `url("${icon}")`,
+								maskSize: "contain",
+								maskRepeat: "no-repeat",
+								maskPosition: "center",
+								WebkitMaskImage: `url("${icon}")`,
+								WebkitMaskSize: "contain",
+								WebkitMaskRepeat: "no-repeat",
+								WebkitMaskPosition: "center",
+							}}
+						>
+							<div className="w-full h-full bg-white" />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+	return null;
+};
+
+const renderMarker = (markerComponent) => {
+	console.log(markerComponent);
+
+	return divIcon({
+		html: renderToString(markerComponent),
+		iconSize: [25, 40],
+		iconAnchor: [25, 25],
+		className: "",
+	});
+};
 
 export const PointList = ({ onMarkerClick }) => {
-  const locations = useSelector((state) => state.locations.locations);
-  const locationsFilters = useSelector((state) => state.locations.filters);
+	const locations = useSelector((state) => state.locations.locations);
+	const locationsFilters = useSelector((state) => state.locations.filters);
 
-  if (!locations) return <></>;
+	if (!locations) return <></>;
 
-  return locations.map(
-    ({ id, place, type }) =>
-      (locationsFilters.type === "all" || locationsFilters.type === type) && (
-        <Marker
-          key={id}
-          position={place.coords}
-          eventHandlers={{ click: () => onMarkerClick(id) }}
-        />
-      )
-  );
+	return locations.map(
+		({ id, place, type }) =>
+			(locationsFilters.type === "all" || locationsFilters.type === type) && (
+				<Marker
+					key={id}
+					position={place.coords}
+					eventHandlers={{ click: () => onMarkerClick(id) }}
+					icon={renderMarker(GetIcon(type))}
+				/>
+			)
+	);
 };
--- src/features/navigation/LocationTypes.jsx
+++ src/features/navigation/LocationTypes.jsx
@@ -8,92 +8,90 @@ import CutleryIcon from "../../assets/icons/cutlery.svg";
 import ParkingIcon from "../../assets/icons/parking.svg";
 
 const LocationType = (props) => {
-  const currentTypeFilter = useSelector(
-    (state) => state.locations.filters.type
-  );
+	const currentTypeFilter = useSelector(
+		(state) => state.locations.filters.type
+	);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  console.log(props.img);
-
-  return (
-    <div
-      className="flex justify-center items-center gap-2 lg:w-8 h-8 border-box py-1 px-4 lg:p-1 rounded-full cursor-pointer"
-      style={{
-        backgroundColor:
-          currentTypeFilter === props.type ? "white" : props.color,
-        border: `2px solid ${props.color}`,
-      }}
-      onClick={() => {
-        if (currentTypeFilter === props.type) {
-          dispatch(clearTypeFilter());
-        } else {
-          dispatch(filterType(props.type));
-        }
-      }}
-    >
-      <div
-        className="inline-block min-h-full h-full aspect-square"
-        style={{
-          maskImage: `url("${props.img}")`,
-          WebkitMaskImage: `url("${props.img}")`,
-          maskSize: "contain",
-          WebkitMaskSize: "contain",
-          maskPosition: "center",
-          WebkitMaskPosition: "center",
-          maskRepeat: "no-repeat",
-          WebkitMaskRepeat: "no-repeat",
-        }}
-      >
-        <div
-          className="w-full h-full"
-          style={{
-            backgroundColor:
-              currentTypeFilter === props.type ? props.color : "white",
-          }}
-        />
-      </div>
-      <span
-        className="lg:hidden whitespace-nowrap"
-        style={{
-          color: currentTypeFilter === props.type ? props.color : "white",
-        }}
-      >
-        {props.alt}
-      </span>
-    </div>
-  );
+	return (
+		<div
+			className="flex justify-center items-center gap-2 lg:w-8 h-8 border-box py-1 px-4 lg:p-1 rounded-full cursor-pointer"
+			style={{
+				backgroundColor:
+					currentTypeFilter === props.type ? "white" : props.color,
+				border: `2px solid ${props.color}`,
+			}}
+			onClick={() => {
+				if (currentTypeFilter === props.type) {
+					dispatch(clearTypeFilter());
+				} else {
+					dispatch(filterType(props.type));
+				}
+			}}
+		>
+			<div
+				className="inline-block min-h-full h-full aspect-square"
+				style={{
+					maskImage: `url("${props.img}")`,
+					WebkitMaskImage: `url("${props.img}")`,
+					maskSize: "contain",
+					WebkitMaskSize: "contain",
+					maskPosition: "center",
+					WebkitMaskPosition: "center",
+					maskRepeat: "no-repeat",
+					WebkitMaskRepeat: "no-repeat",
+				}}
+			>
+				<div
+					className="w-full h-full"
+					style={{
+						backgroundColor:
+							currentTypeFilter === props.type ? props.color : "white",
+					}}
+				/>
+			</div>
+			<span
+				className="lg:hidden whitespace-nowrap"
+				style={{
+					color: currentTypeFilter === props.type ? props.color : "white",
+				}}
+			>
+				{props.alt}
+			</span>
+		</div>
+	);
 };
 
 export const LocationTypes = (props) => {
-  const sideBarOpen = useSelector((state) => state.sideBar.opened);
+	const sideBarOpen = useSelector((state) => state.sideBar.opened);
 
-  return (
-    <div
-      className={`${sideBarOpen ? "hidden" : "flex"} ${
-        props.className ? props.className : ""
-      } px-8 lg:p-0 h-full min-h-[3rem] lg:flex gap-3 items-center overflow-x-scroll lg:overflow-x-auto`}
-    >
-      <LocationType
-        color="#ED8B16"
-        img={UmbrellaIcon}
-        alt="rest in piss"
-        type="beach"
-      />
-      <LocationType color="#C2BB00" img={WeedIcon} alt="SPA" type="spa" />
-      <LocationType color="#E1523D" img={TreeIcon} alt="greenery" type="park" />
-      <LocationType
-        color="#005E54"
-        img={CutleryIcon}
-        alt="Restaurants"
-        type="restaurant"
-      />
-      <LocationType
-        color="#003547"
-        img={ParkingIcon}
-        alt="Parking"
-        type="parking"
-      />
-    </div>
-  );
+	return (
+		<div
+			className={`${sideBarOpen ? "hidden" : "flex"} ${
+				props.className ? props.className : ""
+			} px-8 lg:p-0 h-full min-h-[3rem] lg:flex gap-3 items-center overflow-x-scroll lg:overflow-x-auto`}
+		>
+			<LocationType
+				color="#ED8B16"
+				img={UmbrellaIcon}
+				alt="rest in piss"
+				type="beach"
+			/>
+			<LocationType color="#C2BB00" img={WeedIcon} alt="SPA" type="spa" />
+			<LocationType color="#E1523D" img={TreeIcon} alt="greenery" type="park" />
+			<LocationType
+				color="#005E54"
+				img={CutleryIcon}
+				alt="Restaurants"
+				type="restaurant"
+			/>
+			<LocationType
+				color="#003547"
+				img={ParkingIcon}
+				alt="Parking"
+				type="parking"
+			/>
+		</div>
+	);
 };
