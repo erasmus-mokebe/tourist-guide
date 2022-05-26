407b65ef842895691d1f400ff5f7efde23dda3ec
bk20dev <itsbk20@gmail.com>
2022-05-26 10:48:47 +0300

    Create path slice Render path list based on app state

--- src/pages/Paths.jsx
+++ src/pages/Paths.jsx
@@ -1,19 +1,37 @@
 import { PathCard } from "../features/paths/PathCard";
-
 import PathImage from "../assets/img/paths/beach.jpg";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPaths } from "../store/slices/pathSlice";
 
-const Paths = () => (
-  <main className="grid grid-cols-3 gap-12 mx-12 mt-10">
-    {new Array(10).fill(null).map((_, id) => (
-      <PathCard
-        key={id}
-        img={PathImage}
-        name="pp"
-        description="It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-      />
-    ))}
-  </main>
-);
+const Paths = () => {
+  const dispatch = useDispatch();
+  const paths = useSelector((state) => state.paths.paths);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch("http://localhost:8080/paths");
+      const paths = await data.json();
+      dispatch(setPaths(paths));
+    })();
+  }, []);
+
+  console.log(paths)
+
+  return (
+    <main className="grid grid-cols-3 gap-12 mx-12 mt-10">
+      {(paths || []).map((path) => (
+        <PathCard
+          key={path.id}
+          img={path.img}
+          name={path.name}
+          description={path.description}
+          // TODO: Add missing images
+          // TODO: Add progressbar
+        />
+      ))}
+    </main>
+  );
+};
 
 export default Paths;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import locationsReducer from './slices/locationsSlice';
-import sideBarReducer from './slices/sideBarSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import locationsReducer from "./slices/locationsSlice";
+import sideBarReducer from "./slices/sideBarSlice";
+import pathReducer from "./slices/pathSlice";
 
 export const store = configureStore({
   reducer: {
     locations: locationsReducer,
-    sideBar: sideBarReducer
-  }
+    sideBar: sideBarReducer,
+    paths: pathReducer,
+  },
 });
