737a91b506d098c7f070e38faaac401df90e599c
Pietrek14 <dawidekpe@gmail.com>
2022-05-26 10:32:59 +0300

    back arrow now works as intended

--- src/pages/Browser.jsx
+++ src/pages/Browser.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { openSideBar } from "../store/slices/sideBarSlice";
 import { useParams } from "react-router-dom";
+import { Navigation } from "../features/navigation/Navigation";
 
 export const Browser = (props) => {
   const dispatch = useDispatch();
@@ -13,9 +14,15 @@ export const Browser = (props) => {
   }, [locationId]);
 
   return (
-    <div className="flex-1 flex">
-      {props.children}
-      <Map />
-    </div>
+    <>
+      <Navigation />
+
+      <main className="flex-1 flex h-full overflow-y-hidden">
+        <div className="flex-1 flex">
+          {props.children}
+          <Map />
+        </div>
+      </main>
+    </>
   );
 };
